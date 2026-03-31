a1d01b5a2b62e86e664ba68a23e9ebae687a61d5
Farshadhvd <fhasnavand.business@gmail.com>
2026-03-31 18:10:40 -0500

    Added InMemoryUserDetailsManager to 05-security project

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager getInMemoryUserDetailsManager() {
+        UserDetails userDetails = User.builder().username("user").password("{noop}password").roles("EMPLOYEE").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
